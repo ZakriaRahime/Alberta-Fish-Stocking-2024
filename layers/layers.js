af980a3c9a462a59d47d3d5e76d06bce153f0bc2
ZakriaRahime <zakria.rahime@mail.utoronto.ca>
2024-08-08 02:25:37 -0400

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,19 +1,19 @@
 var wms_layers = [];
 
-var format_Alberta_0 = new ol.format.GeoJSON();
-var features_Alberta_0 = format_Alberta_0.readFeatures(json_Alberta_0, 
+var format_AlbertaBoundary_0 = new ol.format.GeoJSON();
+var features_AlbertaBoundary_0 = format_AlbertaBoundary_0.readFeatures(json_AlbertaBoundary_0, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Alberta_0 = new ol.source.Vector({
+var jsonSource_AlbertaBoundary_0 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Alberta_0.addFeatures(features_Alberta_0);
-var lyr_Alberta_0 = new ol.layer.Vector({
+jsonSource_AlbertaBoundary_0.addFeatures(features_AlbertaBoundary_0);
+var lyr_AlbertaBoundary_0 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Alberta_0, 
-                style: style_Alberta_0,
-                popuplayertitle: "Alberta",
+                source:jsonSource_AlbertaBoundary_0, 
+                style: style_AlbertaBoundary_0,
+                popuplayertitle: "Alberta Boundary",
                 interactive: false,
-                title: '<img src="styles/legend/Alberta_0.png" /> Alberta'
+                title: '<img src="styles/legend/AlbertaBoundary_0.png" /> Alberta Boundary'
             });
 var format_Water_1 = new ol.format.GeoJSON();
 var features_Water_1 = format_Water_1.readFeatures(json_Water_1, 
@@ -53,17 +53,17 @@ var lyr_FishStocking2024_2 = new ol.layer.Vector({
     <img src="styles/legend/FishStocking2024_2_6.png" /> 110000 - 1613075<br />'
         });
 
-lyr_Alberta_0.setVisible(true);lyr_Water_1.setVisible(true);lyr_FishStocking2024_2.setVisible(true);
-var layersList = [lyr_Alberta_0,lyr_Water_1,lyr_FishStocking2024_2];
-lyr_Alberta_0.set('fieldAliases', {'PRUID': 'PRUID', 'DGUID': 'DGUID', 'PRNAME': 'PRNAME', 'PRENAME': 'PRENAME', 'PRFNAME': 'PRFNAME', 'PREABBR': 'PREABBR', 'PRFABBR': 'PRFABBR', 'LANDAREA': 'LANDAREA', });
+lyr_AlbertaBoundary_0.setVisible(true);lyr_Water_1.setVisible(true);lyr_FishStocking2024_2.setVisible(true);
+var layersList = [lyr_AlbertaBoundary_0,lyr_Water_1,lyr_FishStocking2024_2];
+lyr_AlbertaBoundary_0.set('fieldAliases', {'PRUID': 'PRUID', 'DGUID': 'DGUID', 'PRNAME': 'PRNAME', 'PRENAME': 'PRENAME', 'PRFNAME': 'PRFNAME', 'PREABBR': 'PREABBR', 'PRFABBR': 'PRFABBR', 'LANDAREA': 'LANDAREA', });
 lyr_Water_1.set('fieldAliases', {'HYDROUID': 'HYDROUID', 'NAME': 'NAME', 'RANK': 'RANK', 'PRUID': 'PRUID', });
 lyr_FishStocking2024_2.set('fieldAliases', {'DISTRICT': 'DISTRICT', 'WATERBODY': 'WATERBODY', 'Latitude': 'Latitude', 'Longitude': 'Longitude', 'SPECIES': 'SPECIES', 'GENOTYPE': 'GENOTYPE', 'STOCKED -': 'STOCKED -', 'STOCKING N': 'STOCKING N', 'PLANNED ST': 'PLANNED ST', 'Map Author': 'Map Author', 'NumStock': 'NumStock', });
-lyr_Alberta_0.set('fieldImages', {'PRUID': 'TextEdit', 'DGUID': 'TextEdit', 'PRNAME': 'TextEdit', 'PRENAME': 'TextEdit', 'PRFNAME': 'TextEdit', 'PREABBR': 'TextEdit', 'PRFABBR': 'TextEdit', 'LANDAREA': 'TextEdit', });
+lyr_AlbertaBoundary_0.set('fieldImages', {'PRUID': 'TextEdit', 'DGUID': 'TextEdit', 'PRNAME': 'TextEdit', 'PRENAME': 'TextEdit', 'PRFNAME': 'TextEdit', 'PREABBR': 'TextEdit', 'PRFABBR': 'TextEdit', 'LANDAREA': 'TextEdit', });
 lyr_Water_1.set('fieldImages', {'HYDROUID': 'TextEdit', 'NAME': 'TextEdit', 'RANK': 'Range', 'PRUID': 'TextEdit', });
 lyr_FishStocking2024_2.set('fieldImages', {'DISTRICT': 'TextEdit', 'WATERBODY': 'TextEdit', 'Latitude': 'TextEdit', 'Longitude': 'TextEdit', 'SPECIES': 'TextEdit', 'GENOTYPE': 'TextEdit', 'STOCKED -': 'TextEdit', 'STOCKING N': 'TextEdit', 'PLANNED ST': 'TextEdit', 'Map Author': 'TextEdit', 'NumStock': 'Range', });
-lyr_Alberta_0.set('fieldLabels', {'PRUID': 'no label', 'DGUID': 'no label', 'PRNAME': 'no label', 'PRENAME': 'no label', 'PRFNAME': 'no label', 'PREABBR': 'no label', 'PRFABBR': 'no label', 'LANDAREA': 'no label', });
+lyr_AlbertaBoundary_0.set('fieldLabels', {'PRUID': 'no label', 'DGUID': 'no label', 'PRNAME': 'no label', 'PRENAME': 'no label', 'PRFNAME': 'no label', 'PREABBR': 'no label', 'PRFABBR': 'no label', 'LANDAREA': 'no label', });
 lyr_Water_1.set('fieldLabels', {'HYDROUID': 'no label', 'NAME': 'no label', 'RANK': 'no label', 'PRUID': 'no label', });
-lyr_FishStocking2024_2.set('fieldLabels', {'DISTRICT': 'inline label - visible with data', 'WATERBODY': 'inline label - visible with data', 'Latitude': 'hidden field', 'Longitude': 'hidden field', 'SPECIES': 'inline label - visible with data', 'GENOTYPE': 'inline label - visible with data', 'STOCKED -': 'hidden field', 'STOCKING N': 'hidden field', 'PLANNED ST': 'hidden field', 'Map Author': 'header label - always visible', 'NumStock': 'inline label - always visible', });
+lyr_FishStocking2024_2.set('fieldLabels', {'DISTRICT': 'inline label - visible with data', 'WATERBODY': 'inline label - visible with data', 'Latitude': 'hidden field', 'Longitude': 'hidden field', 'SPECIES': 'inline label - visible with data', 'GENOTYPE': 'inline label - visible with data', 'STOCKED -': 'hidden field', 'STOCKING N': 'hidden field', 'PLANNED ST': 'hidden field', 'Map Author': 'header label - always visible', 'NumStock': 'inline label - visible with data', });
 lyr_FishStocking2024_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
